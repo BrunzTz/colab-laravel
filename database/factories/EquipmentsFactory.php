fb2e99ec837f268225f91db3dc32fdc8fd0e04f2
Bruno Rocha <brunolongo333@hotmail.com>
2021-11-30 01:00:25 -0300

    Criando tela em laravel

---
+++ database/factories/EquipmentsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\equipments;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EquipmentsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = equipments::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlockController;
+use App\Http\Controllers\EquipmentsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/blocks', 
+    [BlockController::class, 'index']
+)->middleware(['auth'])->name('blocks');
+
+Route::get('/equipments', 
+    [EquipmentsController::class, 'index']
+)->middleware(['auth'])->name('equipments');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
